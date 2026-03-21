ec4441550e24313e8cd7aac3cad27c88db171d53
Agusss <asantosv@fi.uba.ar>
2026-03-21 20:50:09 -0300

    Agrego la base del menú

--- menu.sh
+++ menu.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+
+  echo "⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔ ꒰ MENÚ ꒱⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔"
+  echo "╰┈➤. Opción 1)  Crear entorno"
+  echo "╰┈➤. Opción 2)  Correr proceso"
+  echo "╰┈➤. Opción 3)  Alumnos ordenados"
+  echo "╰┈➤. Opción 4)  Notas más altas"
+  echo "╰┈➤. Opción 5)  Nro padrón"
+  echo "╰┈➤. Opción 6)  Salir"
+  echo "⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔"
+  echo "<$FILENAME> ha sido exportado exitosamente (ദി˙ᗜ˙)"
